7ae7d564b3959cc9a8deb31df415147ac2a60534
Daniel Eduardo Flores <danser2002@hotmail.com>
2021-12-29 18:24:53 -0500

    Ejercicio04

--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -1,14 +1,34 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    Realice un proceso que permita ingresar los valores por teclado 
+    para el siguiente arreglo
+    double[][] datos = new double[2][2];
  */
 package paquete4;
+
+import java.util.Scanner;
 
 /**
  *
  * @author reroes
  */
 public class Problema04 {
-    
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        double[][] datos = new double[2][2];
+        double valorIngresado;
+
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.printf("Ingrese valor para la posición [%d][%d]\t: ",
+                        i, j);
+                valorIngresado = entrada.nextInt();
+                datos[i][j] = valorIngresado;
+            }
+            System.out.println(); // salto de línea
+        }
+
+    }
+
 }

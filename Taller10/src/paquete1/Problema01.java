34ed20b3c3edaf5de4b77127ffbab4814cdbd797
Daniel Eduardo Flores <danser2002@hotmail.com>
2021-12-29 17:44:22 -0500

    Ejercicio01

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -15,7 +15,38 @@ public class Problema01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Variables
+
+        String[] estudiantes = {"Jerry Ponce", "Gabriela Lewis", "David Bell"};
+        double[][] notas = {{9, 10, 7, 8},
+                            {10, 5, 7, 9},
+                            {7, 9.4, 10, 9}};
+        double[] promedios = new double[3];
+        
+        double suma;
+        double nota;
+
+        for (int fila = 0; fila < notas.length; fila++) {
+            suma = 0;
+            for (int col = 0; col < notas[fila].length; col++) {
+                nota = notas[fila][col];
+                suma = suma + nota;
+                promedios[fila] = suma / notas[fila].length;
+
+            }
+        }
+        for (int fila = 0; fila < estudiantes.length; fila++) {
+            System.out.printf("Estudiante: %s tiene un promedio de %.2f\n",
+                     estudiantes[fila],
+                     promedios[fila]);
+        }
+
     }
-    
+
 }
+
+/*
+Estudiante: Jerry Ponce tiene un promedio de ?
+Estudiante: Gabriela Lewis tiene un promedio de ?
+Estudiante: David Bell tiene un promedio de ?	
+ */

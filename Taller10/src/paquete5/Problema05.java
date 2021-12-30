051aa5fc1f236c979c07801107c8f8d712b6c41c
Daniel Eduardo Flores <danser2002@hotmail.com>
2021-12-29 19:00:13 -0500

    Ejercicio05

--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Imprima en pantalla a través de una variable acumuladora 
+solo los estudiantes que tienen como primera letra S,P,T
  */
 package paquete5;
 
@@ -10,5 +9,35 @@ package paquete5;
  * @author reroes
  */
 public class Problema05 {
-    
+
+    public static void main(String[] args) {
+        //Variables
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+        {"Carrie Burton", "Lauren Rice"},
+        {"Emma Escobar", "Lori Flores"},
+        {"Steven West", "Toni Martin"}};
+
+        String nombre;
+        String iniciales;
+        String cadena = "";
+
+        char[] arregloc = new char[5];
+
+        for (int i = 0; i < estudiantes.length; i++) {
+            for (int j = 0; j < estudiantes[i].length; j++) {
+                nombre = estudiantes[i][j];
+                iniciales = nombre.substring(0,1);
+                if (iniciales.equals("S") || iniciales.equals("P") 
+                        || iniciales.equals("T")) {
+                    cadena = String.format("%s %s\n",
+                            cadena, estudiantes[i][j]);
+                }
+
+            }
+         
+        }
+        System.out.println(cadena);
+        
+    }
+
 }
